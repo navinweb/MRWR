f7a98b26008e3165d728f82b784fa0b6f7c689bb
navinweb <anatoliykirzo@gmail.com>
2018-07-02 13:33:40 +0300

    fix submodule

---
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  const videos = props.videos;
+  const videoItems = videos.map((video) => {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        video={video}
+        key={video.etag}
+      />
+    )
+  })
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  )
+}
+
+export default VideoList;
---
+++ ReduxSimpleStarter/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+};
