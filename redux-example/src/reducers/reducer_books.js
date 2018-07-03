4754f35a1c040c861f8b6c7383e7e7ce2e24147b
navinweb <anatoliykirzo@gmail.com>
2018-07-03 14:17:15 +0300

    add redux

---
+++ redux-example/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    { title: 'books title 1' },
+    { title: 'books title 2' },
+    { title: 'books title 3' },
+    { title: 'books title 4' },
+    { title: 'books title 5' },
+  ]
+}
