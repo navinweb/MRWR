558f54961e7453a362a725dbd277b8e24bbf7e6b
navinweb <anatoliykirzo@gmail.com>
2018-07-03 16:42:30 +0300

    redux tuts over

--- redux-example/src/containers/book_detail.js
+++ redux-example/src/containers/book_detail.js
@@ -3,9 +3,14 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+
+    if (!this.props.book) return <div>Select a book to show details</div>
+
     return (
       <div>
-        Book Detail.
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
       </div>
     )
   }
--- redux-example/src/reducers/reducer_books.js
+++ redux-example/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
   return [
-    { title: 'books title 1' },
-    { title: 'books title 2' },
-    { title: 'books title 3' },
-    { title: 'books title 4' },
-    { title: 'books title 5' },
+    { title: 'books title 1', pages: 100 },
+    { title: 'books title 2', pages: 120 },
+    { title: 'books title 3', pages: 150 },
+    { title: 'books title 4', pages: 250 },
+    { title: 'books title 5', pages: 330 },
   ]
 }
