93be0ed3016295ecc4daca356965b785bac0d93e
navinweb <anatoliykirzo@gmail.com>
2018-07-05 23:37:13 +0300

    set sate for input

--- weather-app/src/containers/search_bar.js
+++ weather-app/src/containers/search_bar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({
+      term: e.target.value
+    })
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          onChange={this.onInputChange}
+          value={this.state.term}
+          placeholder="Get a 5-day forecast in City"
+          className="form-control"
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
