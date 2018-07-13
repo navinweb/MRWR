ce31e58648c59788e912a8f0c21d06e56590c6ca
navinweb <anatoliykirzo@gmail.com>
2018-07-13 21:02:55 +0300

    add weather list

---
+++ weather-app/src/containers/weather_list.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Tempature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return {
+    weather
+  }
+}
+
+export default connect(mapStateToProps)(WeatherList);
