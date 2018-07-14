ccc2c367d3579b29814555a0763fa3eb0ce3cb1f
navinweb <anatoliykirzo@gmail.com>
2018-07-14 17:07:29 +0300

    city list

--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,7 +23,7 @@ export default class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
