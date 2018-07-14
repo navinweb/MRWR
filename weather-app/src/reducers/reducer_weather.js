fd9047c211d8d42440954fbf04d408032e297f00
navinweb <anatoliykirzo@gmail.com>
2018-07-14 17:31:05 +0300

    add sparklines

--- weather-app/src/reducers/reducer_weather.js
+++ weather-app/src/reducers/reducer_weather.js
@@ -3,11 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function (state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.concat[(action.payload.data)];
-      break;
-
-    default:
-      break;
+      return [ action.payload.data, ...state];
   }
   return state;
 }
