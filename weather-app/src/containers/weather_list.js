cefe57112aeeac6e260b2b898ed9d921aefbd43a
navinweb <anatoliykirzo@gmail.com>
2018-07-15 12:57:45 +0300

    add humidity and pressure

--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -6,14 +6,16 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     console.log(cityData.city.name);
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
+    const pressures = _.map(cityData.list.map(weather => weather.main.pressure), (pressure) => pressure*0.75);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units="°C" /></td>
+        <td><Chart data={pressures} color="green" units="mm Hg" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     )
   }
@@ -24,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Tempature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Tempature (°C)</th>
+            <th>Pressure (mm Hg)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
