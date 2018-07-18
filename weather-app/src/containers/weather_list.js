de25186cccda6bcabadef53b128be04c2de5d5ba
navinweb <anatoliykirzo@gmail.com>
2018-07-18 23:24:16 +0300

    google map finish

--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
-    console.log(cityData.city.name);
     const name = cityData.city.name;
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
-    const pressures = _.map(cityData.list.map(weather => weather.main.pressure), (pressure) => pressure*0.75);
+    const pressures = _.map(cityData.list.map(weather => weather.main.pressure), (pressure) => pressure * 0.75);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="°C" /></td>
         <td><Chart data={pressures} color="green" units="mm Hg" /></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
