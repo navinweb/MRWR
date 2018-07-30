67056c2fb272562c18c2f4fa2603144b3c249f3a
navinweb <anatoliykirzo@gmail.com>
2018-07-30 14:35:00 +0300

    add create post action

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=navinweb';
@@ -9,6 +10,15 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
