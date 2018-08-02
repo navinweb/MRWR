6451f996e538a077f9c8c681ded82c8cbda3fcd6
navinweb <anatoliykirzo@gmail.com>
2018-08-02 20:30:40 +0300

    finish

--- blog/src/reducers/reducer_posts.js
+++ blog/src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -8,6 +8,9 @@ export default (state = {}, action) => {
 
     case FETCH_POST:
       return { ...state, [action.payload.data.id]: action.payload.data };
+
+    case DELETE_POST:
+      return _.omit(state, action.payload);
 
     default:
       return state;
